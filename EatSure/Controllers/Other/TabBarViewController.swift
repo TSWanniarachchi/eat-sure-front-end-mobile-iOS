c7b08f32f15ebb04115e8234cb20d00890118100
Sachin Wanniarachchi <sachinwanniarachchi@gmail.com>
2023-02-01 22:35:22 -0800

    Updated Tab bar navigation panel

--- EatSure/Controllers/Other/TabBarViewController.swift
+++ EatSure/Controllers/Other/TabBarViewController.swift
@@ -16,10 +16,6 @@ class TabBarViewController: UITabBarController {
         let vc2 = FavouriteViewController()
         let vc3 = ProfileViewController()
         
-        vc1.title = "Home"
-        vc2.title = "Favourite"
-        vc3.title = "Profile"
-        
         vc1.navigationItem.largeTitleDisplayMode = .always
         vc2.navigationItem.largeTitleDisplayMode = .always
         vc3.navigationItem.largeTitleDisplayMode = .always
@@ -29,7 +25,7 @@ class TabBarViewController: UITabBarController {
         let nav3 = UINavigationController(rootViewController: vc3)
         
         nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
-        nav2.tabBarItem = UITabBarItem(title: "Favourite", image: UIImage(systemName: "heart.circle.fill"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Favourites", image: UIImage(systemName: "heart.circle.fill"), tag: 1)
         nav3.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.crop.circle"), tag: 1)
         
         nav1.navigationBar.prefersLargeTitles = true
